c52a41402b8db394e8a25133435de3207e44805a
failoneverything <failoneverything@gmail.com>
2022-08-10 15:36:04 +0800

    finish lru replacer

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,49 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) : size_{num_pages} {}
 
 LRUReplacer::~LRUReplacer() = default;
 
-auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool { return false; }
+auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool {
+  std::scoped_lock<std::mutex> lock(latch_);
+  if (lru_list_.size() == 0) {
+    return false;
+  }
+  *frame_id = lru_list_.back();
+  lru_list_.pop_back();
+  lru_map_.erase(*frame_id);
+  return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(latch_);
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+  auto iter = lru_map_.find(frame_id);
+  if (iter != lru_map_.end()) {
+    lru_list_.erase(iter->second);
+    lru_map_.erase(frame_id);
+  }
+}
 
-auto LRUReplacer::Size() -> size_t { return 0; }
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> lock(latch_);
+
+  auto iter = lru_map_.find(frame_id);
+  if (iter == lru_map_.end()) {
+    lru_list_.push_front(frame_id);
+    lru_map_[frame_id] = lru_list_.begin();
+    if (lru_map_.size() > this->size_) {
+      frame_id_t last = lru_list_.back();
+      lru_list_.pop_back();
+      lru_map_.erase(last);
+    }
+  }
+}
+
+auto LRUReplacer::Size() -> size_t {
+  std::scoped_lock<std::mutex> lock(latch_);
+  return this->lru_list_.size();
+}
 
 }  // namespace bustub
